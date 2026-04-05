c16d4e965bfb9007c4473c226fc30a817de67f80
govindpatel <govindpatel2510734@gmail.com>
2026-04-05 19:14:50 +0530

    updated api backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://file-arranger-system.onrender.com',
 });
 
 export const uploadFiles = (formData) => API.post('/upload', formData);
